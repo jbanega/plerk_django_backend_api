c0a76a5fb601aafbc94102cf770e65b12c0c1ca4
jbanega <jban3ga@gmail.com>
2022-01-25 01:18:52 -0400

    App first version completed

--- APIapp/tests.py
+++ APIapp/tests.py
@@ -12,11 +12,27 @@ class HomePageTests(SimpleTestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, "home.html")
+        self.assertTemplateUsed(self.response, "index.html")
 
     def test_homepage_contains_correct_html(self):
-        self.assertContains(self.response, "Plerk Django Backend API")
+        self.assertContains(self.response, "plerk RESTful API")
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(
         self.response, "This should not be on the homepage.")
+
+
+class ResponsePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse("response")
+        self.response = self.client.get(url)
+
+    def test_responsehome_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_response_template(self):
+        self.assertTemplateUsed(self.response, "response.html")
+
+    def test_response_contains_correct_html(self):
+        self.assertContains(self.response, "J")
