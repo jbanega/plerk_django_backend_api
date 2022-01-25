55981adcd420523587ff6177d8978373e2d29e2a
jbanega <jban3ga@gmail.com>
2022-01-25 01:31:53 -0400

    Ready for deployment

--- plerkAPI/settings.py
+++ plerkAPI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 import environ
 import os
 from pathlib import Path
@@ -148,3 +149,7 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
